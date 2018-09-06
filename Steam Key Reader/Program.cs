213f83eb705f396cef311a118ed0c4ae6a1647ac
Colton Horvath <coltonrhorvath@gmail.com>
2018-09-06 14:47:31 -0400

    Steam Key Reader
    This is the beginning of a program that when ran, willl scan websites for Steam game keys. The Steam API has been imported as well. The program will be able to scan webpages and submit them to steam on it's own with your own log in information.
    
    I'm currently working on getting regex to work via reddit's homepage.

--- Steam Key Reader/Program.cs
+++ Steam Key Reader/Program.cs
@@ -3,13 +3,48 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data;
+using System.Net;
+using System.IO;
+using System.Text.RegularExpressions;
+using SteamKit2;
 
 namespace Steam_Key_Reader
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
+            WebClient webClient = new WebClient();
+            webClient.OpenReadCompleted += new OpenReadCompletedEventHandler(webClient_OpenReadCompleted);
+            Uri URL = new Uri("https://reddit.com");
+            webClient.OpenReadAsync(URL);
+            Console.ReadKey();
+        }
+
+        public static void webClient_OpenReadCompleted(object sender, OpenReadCompletedEventArgs e)
+        {
+            if (e.Error != null)
+            {
+                Console.WriteLine(e.Error.Message);
+            }
+            else
+            {
+                //@"a href=""(?<link>.+?)"""
+                const string pattern = @"([help])";
+                Regex regex = new Regex(pattern, RegexOptions.IgnoreCase);
+                TextReader tR = new StreamReader(e.Result);
+                string content = tR.ReadToEnd();
+                MatchCollection mC = regex.Matches(content);
+
+                foreach (Match match in mC)
+                {
+                    Console.WriteLine(match.Value);
+                }
+                tR.Close();
+                Console.WriteLine("Done");
+                Console.ReadKey();
+            }
         }
     }
 }
